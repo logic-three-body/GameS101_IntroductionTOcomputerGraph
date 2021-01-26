8257b1f1a9bfc009817725a7f5b38245f1fc819a
logic-three-body <1552451580@qq.com>
2021-01-26 13:18:00 +0800

    getIntersection IntersectP(const Ray& ray, const Vector3f& invDir, const std::array<int, 3>& dirIsNeg)

--- Whitted Style Ray Tracing Accelerate/BVH.cpp
+++ Whitted Style Ray Tracing Accelerate/BVH.cpp
@@ -1,4 +1,4 @@
-#include <algorithm>
+﻿#include <algorithm>
 #include <cassert>
 #include "BVH.hpp"
 
@@ -105,5 +105,27 @@ Intersection BVHAccel::Intersect(const Ray& ray) const
 Intersection BVHAccel::getIntersection(BVHBuildNode* node, const Ray& ray) const
 {
     // TODO Traverse the BVH to find intersection
-	return Intersection();
+	Intersection isect;
+
+	std::array<int, 3> dirIsNeg;
+	dirIsNeg[0] = int(ray.direction.x >= 0);
+	dirIsNeg[1] = int(ray.direction.y >= 0);
+	dirIsNeg[2] = int(ray.direction.z >= 0);
+
+	if (!node->bounds.IntersectP(ray, ray.direction_inv, dirIsNeg))
+	{
+		return isect;
+	}
+
+	// 叶子节点
+	if (node->left == nullptr && node->right == nullptr)
+	{
+		isect = node->object->getIntersection(ray);
+		return isect;
+	}
+
+	auto hit1 = getIntersection(node->left, ray);
+	auto hit2 = getIntersection(node->right, ray);
+
+	return hit1.distance < hit2.distance ? hit1 : hit2;
 }
