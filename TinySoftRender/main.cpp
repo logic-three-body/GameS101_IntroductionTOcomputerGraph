750344f4e2585f38b519252279cb4e73547290b5
logic-three-body <1552451580@qq.com>
2021-04-20 16:45:39 +0800

    测试read函数，output2为未垂直反转，output3为垂直反转flip_vertically

--- TinySoftRender/main.cpp
+++ TinySoftRender/main.cpp
@@ -5,8 +5,9 @@ const TGAColor green = TGAColor(0, 255, 0, 255);
 int main()
 {
 	TGAImage image(100, 100, TGAImage::RGB);
-	image.set(0, 0, white);//为设置坐标，此处为白点为原点
-	//image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-	image.write_tga_file("output2.tga");
+	image.read_tga_file("obj/african_head/african_head_diffuse.tga");
+	//image.set(0, 0, white);//为设置坐标，此处为白点为原点
+	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+	image.write_tga_file("output3.tga");
 	return 0;
 }
