cb6b9b0056c211482d0bdba8ecd4f267f91f9ca7
logic-three-body <1552451580@qq.com>
2021-04-23 20:42:33 +0800

    画线函数的问题

--- TinySoftRender/main.cpp
+++ TinySoftRender/main.cpp
@@ -1,35 +1,64 @@
-#include"TGAImage.h"
+#include <vector>
+#include <cmath>
+#include "tgaimage.h"
+#include "model.h"
+#include "geometry.h"
 #include"line.h"
-#include"global.h"
-#include"model.h"
-#include<cmath>
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+Model *model = NULL;
+const int width = 800;
+const int height = 800;
 
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+	bool steep = false;
+	if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
+		std::swap(x0, y0);
+		std::swap(x1, y1);
+		steep = true;
+	}
+	if (x0 > x1) {
+		std::swap(x0, x1);
+		std::swap(y0, y1);
+	}
 
-
-
-int main()
-{
-	Model *model = nullptr;
-	model = new Model("obj/african_head/african_head.obj");
-	TGAImage img(width, height, TGAImage::RGB);
-	for (int i = 0; i < model->nfaces(); i++)
-	{
-		std::vector<int> face = model->face(i);
-		for (int j = 0; j < 3; j++)
-		{
-			Vec3f v0 = model->vert(face[j]);
-			Vec3f v1 = model->vert(face[(j+1)%3]);
-			int x0 = (v0.x+1.0)*width/2.0;
-			int y0 = (v0.y + 1.0)*height / 2.0;
-			int x1 = (v1.x + 1.0)*width / 2.0;
-			int y1 = (v1.y + 1.0)*height / 2.0;
-			lineBresenham(x0,y0,x1,y1,img,white);
+	for (int x = x0; x <= x1; x++) {
+		float t = (x - x0) / (float)(x1 - x0);
+		int y = y0 * (1. - t) + y1 * t;
+		if (steep) {
+			image.set(y, x, color);
+		}
+		else {
+			image.set(x, y, color);
 		}
 	}
-	img.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-	img.write_tga_file("Lesson1Line/Wireframe/output.tga");
+}
+
+int main(int argc, char** argv) {
+	if (2 == argc) {
+		model = new Model(argv[1]);
+	}
+	else {
+		model = new Model("obj/african_head/african_head.obj");
+	}
+
+	TGAImage image(width, height, TGAImage::RGB);
+	for (int i = 0; i < model->nfaces(); i++) {
+		std::vector<int> face = model->face(i);
+		for (int j = 0; j < 3; j++) {
+			Vec3f v0 = model->vert(face[j]);
+			Vec3f v1 = model->vert(face[(j + 1) % 3]);
+			int x0 = (v0.x + 1.)*width / 2.;
+			int y0 = (v0.y + 1.)*height / 2.;
+			int x1 = (v1.x + 1.)*width / 2.;
+			int y1 = (v1.y + 1.)*height / 2.;
+			//line(x0, y0, x1, y1, image, white);
+			lineBresenham(x0, y0, x1, y1, image, white);
+		}
+	}
+
+	//image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+	image.write_tga_file("Lesson1Line/Wireframe/output2.tga");
 	delete model;
 	return 0;
 }
-
-
