8ffd129a89aa3feef4d8734c2aa9b0b5585c4291
logic-three-body <1552451580@qq.com>
2021-04-23 21:12:10 +0800

    更正错误，见更改

--- TinySoftRender/line.cpp
+++ TinySoftRender/line.cpp
@@ -43,7 +43,7 @@ void lineBresenham(int x0, int y0, int x1, int y1, TGAImage & img, const TGAColo
 		std::swap(x0, x1);
 		std::swap(y0, y1);
 	}
-	for (int x = 0; x <= x1; x++)
+	for (int x = x0; x <= x1; x++)
 	{
 		float t = (x - x0) / float(x1 - x0);
 		int y = y0 * (1.0f - t) + y1 * t;
