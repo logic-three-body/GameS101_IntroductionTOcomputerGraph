09e69a40e7ddf5bdb20792941a95e2a7b49d0433
logic-three-body <1552451580@qq.com>
2021-04-26 14:04:09 +0800

    更改程序结构

--- TinySoftRender/main.cpp
+++ TinySoftRender/main.cpp
@@ -1,11 +1,7 @@
 #include"FrameWork.h"
-
+#include"global.h"
 int main(int argc, char** argv) {
-	///const char*new_path = "obj/diablo3_pose/diablo3_pose.obj";
-	///model.clear();
-	///model.Update(new_path);
 	rasterizer r(800,800);
-	//DrawWireframe(model);
 	r.DrawWireFrame(model,image,green);
 	image.write_tga_file("Lesson1Line/Wireframe/attempt/rasterizer/output.tga");
 	return 0;
