a6863c73eac3022dc13d0529ff51b73d4ed4ce6a
logic-three-body <1552451580@qq.com>
2021-04-26 19:39:55 +0800

    测试三角形

--- TinySoftRender/Value.h
+++ TinySoftRender/Value.h
@@ -8,6 +8,19 @@ typedef struct Trianglei
 public:
 	Trianglei():p0(Vec2i(0,0)), p1(Vec2i(0, 0)), p2(Vec2i(0, 0)) {};
 	Trianglei(Vec2i _p0, Vec2i _p1, Vec2i _p2) :p0(_p0), p1(_p1), p2(_p2) {};
+	Trianglei(Vec2i*vi)
+	{
+		if (vi)
+		{
+			p0 = vi[0];
+			p1 = vi[1];
+			p2 = vi[2];
+		}
+		else
+		{
+			Trianglei();
+		}
+	};
 };
 
 
--- TinySoftRender/main.cpp
+++ TinySoftRender/main.cpp
@@ -1,12 +1,12 @@
 ﻿#include"FrameWork.h"
 #include"global.h"
 int main(int argc, char** argv) {
-	auto path1 = "Lesson2rasterizer/Trangile/trangile.tga";
+	auto path1 = "Lesson2rasterizer/Trangile/trangile2.tga";
 	rasterizer r(width,height);
+	Vec2i pts[3] = { Vec2i(10,10), Vec2i(100, 30), Vec2i(190, 160) };
 	Vec2i t0[3] = { Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80) };
-	Vec2i t1[3] = { Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180) };
-	Vec2i t2[3] = { Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180) };
-	r.DrawFillTrangile(t0[0],t0[1],t0[2],green);
+	Trianglei pt(t0);
+	r.DrawFillTrangile(pt, white);
 	r.WriteBuffer(path1);
 	return 0;
 }
