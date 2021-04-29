2a463869bf0895da084ee81c5ce107834453d9fe
logic-three-body <1552451580@qq.com>
2021-04-29 18:02:57 +0800

    YBuffer

--- TinySoftRender/global.h
+++ TinySoftRender/global.h
@@ -2,8 +2,9 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 const TGAColor green = TGAColor(0, 255, 0, 255);
+const TGAColor blue = TGAColor(0, 0, 255, 255);
 const auto path = "obj/african_head/african_head.obj";
 const int width = 800;
-const int height = 800;
-static Model model(path);
+const int height = 500;
+//static Model model(path);
 static TGAImage image(width, height, TGAImage::RGB);
--- TinySoftRender/main.cpp
+++ TinySoftRender/main.cpp
@@ -1,17 +1,19 @@
 ﻿#include"FrameWork.h"
 #include"global.h"
 int main(int argc, char** argv) {
-	auto path1 = "Lesson2rasterizer/Flat/model7.tga";
-	auto path2 = "Lesson2rasterizer/Flat/model8.tga";
+	auto path1 = "Lesson3Zbuffer/YBuffer/test.tga";
+	auto path2 = "Lesson3Zbuffer/YBuffer/test1.tga";
 	rasterizer r(width,height);
-	Vec2i pts[3] = { Vec2i(10,10), Vec2i(100, 30), Vec2i(190, 160) };
-	Trianglei pt(pts);
-	Vec3f light_dir(0,0,-1);
-	r.DrawGrayFrame(model, light_dir.normalize());
+	r.lineBresenham(Vec2i(20, 34), Vec2i(744, 400), red);
+	r.lineBresenham(Vec2i(120, 434), Vec2i(444, 400), green);
+	r.lineBresenham(Vec2i(330, 463), Vec2i(594, 200), blue);
+
+	r.lineBresenham(Vec2i(10, 10), Vec2i(790, 10), white);
+
 	r.WriteBuffer(path1);
-	r.ClearBuffer();
-	r.DrawFlatFrame(model);
-	r.WriteBuffer(path2);
+	//r.ClearBuffer();
+	//r.DrawFlatFrame(model);
+	//r.WriteBuffer(path2);
 
 	return 0;
 }
