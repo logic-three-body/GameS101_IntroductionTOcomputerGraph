13d6dbe75f70647a6d30ce1030853d8f60a9db33
logic-three-body <1552451580@qq.com>
2021-05-03 22:41:43 +0800

    绘制其他模型会出问题

--- TinySoftRender/main.cpp
+++ TinySoftRender/main.cpp
@@ -1,12 +1,13 @@
 ﻿#include"FrameWork.h"
 #include"global.h"
 int main(int argc, char** argv) {
-	auto path1 = "Lesson4Perspective/Shark/test3.tga";
+	auto path1 = "Lesson4Perspective/Shark/test4.tga";
 	auto path2 = "Lesson4Perspective/Test/test1.tga";
 	rasterizer r(width,height);
 	r.InitZBuffer();
 	r.viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
-	r.Prespect_projection(-1.f / camera.z);
+	//r.Prespect_projection(-1.f / camera.z);
+	r.projection();
 	r.DrawModelFrame(model, light_dir);
 	r.WriteBuffer(path1);
 	//r.ClearBuffer();
