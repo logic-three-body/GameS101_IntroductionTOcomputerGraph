a980f6f76d02b4af43b80b3a8f8de6e8aae2aa68
logic-three-body <1552451580@qq.com>
2021-05-03 20:19:07 +0800

    成功绘制，viewport函数不如直接world2screen效果好 今后继续探究viewport的用处

--- TinySoftRender/main.cpp
+++ TinySoftRender/main.cpp
@@ -12,7 +12,7 @@ mat<4, 1, float> v2m(Vec3f v) {//顶点至矩阵
 	return m;
 }
 int main(int argc, char** argv) {
-	auto path1 = "Lesson4Perspective/Test/test19.tga";
+	auto path1 = "Lesson4Perspective/Test/test20.tga";
 	auto path2 = "Lesson4Perspective/Test/test1.tga";
 	rasterizer r(width,height);
 	r.InitZBuffer();
@@ -25,19 +25,20 @@ int main(int argc, char** argv) {
 		Vec3f world_coords[3];
 		for (int j = 0; j < 3; j++) {
 			Vec3f v = model.vert(face[j]);
+			//viewport函数不如直接world2screen效果好
+			//screen_coords[j] = m2v(r.GetViewPort()*r.GetProjection()*v2m(v));
 			screen_coords[j] = r.world2screen(m2v(r.GetProjection()*v2m(v)));
 			world_coords[j] = v;
 		}
 		Vec3f n = cross((world_coords[2] - world_coords[0]), (world_coords[1] - world_coords[0]));
 		n.normalize();
 		float intensity = n * light_dir;
-		r.DrawInterpolateTrangile(screen_coords[0], screen_coords[1], screen_coords[2], TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
-		//if (intensity > 0) {
-		//	//r.DrawInterpolateTrangile(screen_coords[0], screen_coords[1], screen_coords[2], TGAColor(int(intensity * 255) % 255, int(intensity * 255) % 255, int(intensity * 255) % 255, 255));
-		//	//r.DrawInterpolateTrangile(screen_coords[0], screen_coords[1], screen_coords[2], TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
-		//	r.DrawFillTrangile(screen_coords[0], screen_coords[1], screen_coords[2], TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
-
-		//}
+		//r.DrawInterpolateTrangile(screen_coords[0], screen_coords[1], screen_coords[2], TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
+		if (intensity > 0) {
+			r.DrawInterpolateTrangile(screen_coords[0], screen_coords[1], screen_coords[2], TGAColor(int(intensity * 255) % 255, int(intensity * 255) % 255, int(intensity * 255) % 255, 255));
+			//r.DrawInterpolateTrangile(screen_coords[0], screen_coords[1], screen_coords[2], TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
+			//r.DrawFillTrangile(screen_coords[0], screen_coords[1], screen_coords[2], TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
+		}
 	}
 
 
