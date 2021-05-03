54aab482e8c36baca04cfd5d8e29f068cfedcd9e
logic-three-body <1552451580@qq.com>
2021-05-03 22:49:18 +0800

    灯光方向很重要

--- TinySoftRender/global.h
+++ TinySoftRender/global.h
@@ -9,7 +9,7 @@ const auto path2 = "obj/Jelle.obj";
 const int width = 800;
 const int height = 800;
 const int depth = 255;
-Vec3f light_dir(0,0,1);
+Vec3f light_dir(0,0,-1);
 Vec3f camera(0, 0, 3);
 static Model model(path);
 static TGAImage image(width, height, TGAImage::GRAYSCALE);
