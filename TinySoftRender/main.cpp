413c3b18dab4378493fb352442c4c17ed78c0867
logic-three-body <1552451580@qq.com>
2021-05-04 21:15:05 +0800

    找好相机点位才可以渲染出图

--- TinySoftRender/main.cpp
+++ TinySoftRender/main.cpp
@@ -1,7 +1,7 @@
 ﻿#include"FrameWork.h"
 #include"global.h"
 int main(int argc, char** argv) {
-	auto path1 = "Lesson5Camera/test2.tga";
+	auto path1 = "Lesson5Camera/test4.tga";
 	auto path2 = "Lesson5Camera/test1.tga";
 	rasterizer r(width,height);
 	r.InitZBuffer();
@@ -9,7 +9,7 @@ int main(int argc, char** argv) {
 	float cof= -1.f / (eye - center).norm();
 	r.Prespect_projection(cof);
 	//r.projection();
-	r.lookat(eye, center, Vec3f(0, 1, 0));
+	r.lookat(eye, center, Vec3f(-4.074089 ,1.102792 ,- 0.125490));
 	r.DrawModelFrame(model, light_dir);
 	r.WriteBuffer(path1);
 	//r.ClearBuffer();
