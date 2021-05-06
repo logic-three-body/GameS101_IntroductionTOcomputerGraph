8438802e55508f037227c78473a3f576c8dbded3
logic-three-body <1552451580@qq.com>
2021-05-06 16:22:17 +0800

    测试viewport

--- TinySoftRender/main.cpp
+++ TinySoftRender/main.cpp
@@ -1,7 +1,7 @@
 ﻿#include"FrameWork.h"
 #include"global.h"
 int main(int argc, char** argv) {
-	auto path1 = "TriangleTransform/test.tga";
+	auto path1 = "TriangleTransform/test2.tga";
 	auto path2 = "Lesson5Camera/test1.tga";
 	rasterizer r(width,height);
 	r.InitZBuffer();
@@ -13,11 +13,15 @@ int main(int argc, char** argv) {
 
 
 
-	Vec3f p1(0, 0, 1), p2(1, 0, 0), p3(0, 1, 0);
+	Vec3f p0(0, 0, 1), p1(1, 0, 0), p2(0, 1, 0);
 	Vec3f pt[3];
-	pt[0] = r.world2screen(p1);
-	pt[1] = r.world2screen(p2);
-	pt[3] = r.world2screen(p3);
+	//pt[0] = r.world2screen(p0);
+	//pt[1] = r.world2screen(p1);
+	//pt[3] = r.world2screen(p2);
+
+	pt[0] = r.m2v(r.GetViewPort()*r.v2m(p0));
+	pt[1] = r.m2v(r.GetViewPort()*r.v2m(p1));
+	pt[2] = r.m2v(r.GetViewPort()*r.v2m(p2));
 	Triangle3f t(pt);
 	r.DrawInterpolateTrangile(t,white);
 	//r.DrawModelFrame(model, light_dir);
