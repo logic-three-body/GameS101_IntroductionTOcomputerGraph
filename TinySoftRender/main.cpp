da964a0041cb1ed89574810c98292aacac3e70eb
logic-three-body <1552451580@qq.com>
2021-05-07 20:42:43 +0800

    光源测试

--- TinySoftRender/main.cpp
+++ TinySoftRender/main.cpp
@@ -2,7 +2,7 @@
 #include"global.h"
 int main(int argc, char** argv) {
 	auto path1 = "TriangleTransform/test29.tga";
-	auto path2 = "Lesson5Camera/model5.tga";
+	auto path2 = "Lesson5Camera/model6.tga";
 	rasterizer r(width,height);
 	r.InitZBuffer();
 	r.viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4, depth);
