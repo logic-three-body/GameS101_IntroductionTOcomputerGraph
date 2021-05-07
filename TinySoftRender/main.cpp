bebc7e7cbfa1ee26b921c0d687b21d22c16eb54a
logic-three-body <1552451580@qq.com>
2021-05-07 20:45:01 +0800

    透视灯光

--- TinySoftRender/main.cpp
+++ TinySoftRender/main.cpp
@@ -2,13 +2,13 @@
 #include"global.h"
 int main(int argc, char** argv) {
 	auto path1 = "TriangleTransform/test29.tga";
-	auto path2 = "Lesson5Camera/model6.tga";
+	auto path2 = "Lesson5Camera/modelp.tga";
 	rasterizer r(width,height);
 	r.InitZBuffer();
 	r.viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4, depth);
 	float cof= -1.f / (eye - center).norm();
-	//r.Prespect_projection(cof);
-	r.projection();
+	r.Prespect_projection(cof);
+	//r.projection();
 	float scale = 1.0;
 	//r.lookat(eye, center*scale, up);
 	r.lookatZ();
