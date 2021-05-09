75a9dc25a1af681856be626b420df8baecc69036
logic-three-body <1552451580@qq.com>
2021-05-09 17:00:11 +0800

    更改导出缩放比例 放大10倍 模型显示正常 放大100倍 程序计算时间过长，怀疑有数组越界风险

--- OwnSoftRender/main.cpp
+++ OwnSoftRender/main.cpp
@@ -38,12 +38,15 @@ int main(int argc, char** argv) {
         model = new Model(argv[1]);
     } else {
        // model = new Model("obj/african_head.obj");
-		model = new Model(imread_path_head);
+		model = new Model(imread_path_bunny);
     }
+	//float coef = 10000.0f;
+	//eye = eye * coef;
 
+	
     lookat(eye, center, up);
 	//lookatZ();
-    viewport(width/8, height/8, width*3/4, height*3/4);
+	viewport(width / 8, height / 8, width * 3 / 4, height * 3 / 4);
     projection(-1.f/(eye-center).norm());
 	//projection();
     light_dir.normalize();
@@ -64,8 +67,8 @@ int main(int argc, char** argv) {
     zbuffer.flip_vertically();
     //image.  write_tga_file("Gouraud/output1.tga");
     //zbuffer.write_tga_file("Gouraud/zbuffer1.tga");
-	image.write_tga_file(imwrite_path_head);
-	zbuffer.write_tga_file(imwrite_pathZ_head);
+	image.write_tga_file(imwrite_path_bunny);
+	zbuffer.write_tga_file(imwrite_pathZ_bunny);
     delete model;
     return 0;
 }
